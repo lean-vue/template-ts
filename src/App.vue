bda30955ebddadb80927450cbd16c52012afdd7d
Micha Buchholz <mail@lean-stack.de>
2023-09-25 10:08:06 +0200

    feat: Create initial empty vue app.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<template>
+  <h1>Lean Vue App Template</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  text-align: center;
+  margin-top: 4rem;
+}
+</style>
