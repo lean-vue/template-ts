fa46f2be18785b1a397fb94c297af2b288d0ae53
Micha Buchholz <mail@lean-stack.rocks>
2024-11-12 22:56:06 +0100

    chore: Catch vue file errors.

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,21 @@
 import jsRules from '@eslint/js';
 import tsRules from 'typescript-eslint';
+import vueRules from 'eslint-plugin-vue';
 
 export default [
   {
     name: 'app/opt-in-files',
-    files: ['src/**/*.ts'],
+    files: ['src/**/*.{ts,vue}'],
   },
   jsRules.configs['recommended'],
   ...tsRules.configs['recommended'],
+  ...vueRules.configs['flat/recommended'],
+  {
+    files: ['src/**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+      },
+    },
+  },
 ];
